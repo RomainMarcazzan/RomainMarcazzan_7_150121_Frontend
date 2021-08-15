843f53744279d7f099fb801a4b300c82e6ca6be0
RomainMarcazzan <romain.marcazzan@hotmail.com>
2021-08-15 16:44:04 +0400

    ading netlify.toml

--- src/components/ListOfPosts.js
+++ src/components/ListOfPosts.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import "./ListOfPosts.css";
 import axios from "axios";
 import { PostContext } from "../context/PostContext";
